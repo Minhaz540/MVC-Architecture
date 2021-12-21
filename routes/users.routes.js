8bdd72705270c573ca403d6b49ef269eb458dd2b
Minhaz <sharif35-3001@diu.edu.bd>
2021-12-21 11:57:15 +0600

    Fulfill the mvc structure

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const routes = express.Router();
-const userController = require("../controllers/user.controller");
 const { getUsers, saveUsers } = require("../controllers/users.controller");
 
 routes.get("/", getUsers);
